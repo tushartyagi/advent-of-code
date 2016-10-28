8771f031d2eb886b4340310c794579e69c2d70d3
Tushar Tyagi <tushar.tyagi@cognizant.com>
2016-10-28 15:08:38 +0530

    Day5b

--- cs/Solutions/Day5.cs
+++ cs/Solutions/Day5.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -12,7 +13,7 @@ namespace AdventOfCode.Day5 {
         }
 
         public static bool HasALetterTwiceInARow(string input) {
-            string twiceInARow = @"([a-zA-Z])\1";
+            string twiceInARow = @"(\w)\1";
             var r = new Regex(twiceInARow);
             var result = r.Match(input);
             return result.Success;
@@ -25,24 +26,53 @@ namespace AdventOfCode.Day5 {
                 || input.Contains("xy");
         }
 
-        public static bool IsNice(string input) {
+        public static bool RepeatsInBetween(string input) {
+            string repeatsInBetween = @"(\w)\w\1";
+            var r = new Regex(repeatsInBetween);
+            var result = r.Match(input);
+            return result.Success;
+        }
+
+        public static bool PairsAppearTwice(string input) {
+            string pairsAppearTwice = @"(\w\w)\w*\1";
+            var r = new Regex(pairsAppearTwice);
+            var result = r.Match(input);
+            return result.Success;
+        }
+
+        public static bool IsNice_v1(string input) {
             return Has3Vowels(input) && HasALetterTwiceInARow(input) && !HasSomeStrings(input);
+        }
+
+        public static bool IsNice_v2(string input) {
+            return RepeatsInBetween(input) && PairsAppearTwice(input);
         }
     }
     
     public static class NiceOrNaughty {
 
-        public static void RunPartA() {
+        private static void Run(Func<string, bool> IsNice) {
         
             using(StreamReader stream = new StreamReader(File.Open(@".\Input\Day5.txt", FileMode.Open))) {
                 var niceCount = 0;
                 for(var line = stream.ReadLine(); line != null; line = stream.ReadLine())
                 {
-                    if (Helpers.IsNice(line)) niceCount++;
+                    if (IsNice(line)) niceCount++;
                 }
                 System.Console.WriteLine("The number of nice lines: {0}" , niceCount);
             }        
         }
+
+        public static void RunPartA() {
+        
+            Run(Helpers.IsNice_v1);
+        }
+
+        public static void RunPartB() {
+        
+            Run(Helpers.IsNice_v2);
+        }
+
     }
 
 }
--- cs/Tests/Day5.cs
+++ cs/Tests/Day5.cs
@@ -31,15 +31,43 @@ public class TestDay5_Helpers {
     }
 
     [Test]
-    public void It_Should_Work_Correctly_For_Nice_Strings() {
-        Assert.IsTrue(Helpers.IsNice("ugknbfddgicrmopn"));
-        Assert.IsTrue(Helpers.IsNice("aaa"));
+    public void It_Should_Work_Correctly_For_Nice_Strings_v1() {
+        Assert.IsTrue(Helpers.IsNice_v1("ugknbfddgicrmopn"));
+        Assert.IsTrue(Helpers.IsNice_v1("aaa"));
     }
 
     [Test]
-    public void It_Should_Work_Correctly_For_Naughty_Strings() {
-        Assert.IsFalse(Helpers.IsNice("jchzalrnumimnmnp"));
-        Assert.IsFalse(Helpers.IsNice("haegwjzuvuyypxyu"));
-        Assert.IsFalse(Helpers.IsNice("dvszwmarrgswjxmb"));
+    public void It_Should_Work_Correctly_For_Naughty_Strings_v1() {
+        Assert.IsFalse(Helpers.IsNice_v1("jchzalrnumimnmnp"));
+        Assert.IsFalse(Helpers.IsNice_v1("haegwjzuvuyypxyu"));
+        Assert.IsFalse(Helpers.IsNice_v1("dvszwmarrgswjxmb"));
+    }
+
+    [Test]
+    public void It_Should_Work_Correctly_For_Repeats_In_Between() {
+        Assert.IsTrue(Helpers.RepeatsInBetween("xyx"));
+        Assert.IsTrue(Helpers.RepeatsInBetween("aaa"));
+        Assert.IsTrue(Helpers.RepeatsInBetween("abcdefeghi"));
+        Assert.IsTrue(Helpers.RepeatsInBetween("qjhvhtzxzqqjkmpb"));
+    }
+
+    [Test]
+    public void It_Should_Work_Correctly_For_Pairs_In_Between() {
+        Assert.IsTrue(Helpers.PairsAppearTwice("xyxy"));
+        Assert.IsTrue(Helpers.PairsAppearTwice("aabcdefghaa"));
+        Assert.IsTrue(Helpers.PairsAppearTwice("qjhvhtzxzqqjkmpb"));
+        Assert.IsFalse(Helpers.PairsAppearTwice("aaa"));
+    }
+
+    [Test]
+    public void It_Should_Work_Correctly_For_Nice_Strings_v2() {
+        Assert.IsTrue(Helpers.IsNice_v2("qjhvhtzxzqqjkmpb"));
+        Assert.IsTrue(Helpers.IsNice_v2("xxyxx"));
+    }
+
+    [Test]
+    public void It_Should_Work_Correctly_For_Naughty_Strings_v2() {
+        Assert.IsFalse(Helpers.IsNice_v2("ieodomkazucvgmuy"));
+        Assert.IsFalse(Helpers.IsNice_v2("uurcxstgmygtbstg"));
     }
 }
