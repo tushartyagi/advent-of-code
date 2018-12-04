99fc2800fb8c125bf58e2b7d1773de117e9a3835
Tushar Tyagi <tushar4r@gmail.com>
2018-12-04 09:39:32 +0530

    C#: D3P2

--- 2018/cs/AdventOfCode/Program.cs
+++ 2018/cs/AdventOfCode/Program.cs
@@ -12,21 +12,25 @@ namespace AdventOfCode
             var inputPath = "inputs/3.txt";
             var solution = new Solution3(1000);
 
-            //var solution = new Solution3(8);
-            //var claims = new list<string>
-            //{
-            //    "#1 @ 1,3: 4x4",
-            //    "#2 @ 3,1: 4x4",
-            //    "#3 @ 5,5: 2x2"
-            //};
+            // var solution = new Solution3(8);
+            // var claims = new List<string>
+            // {
+            //     "#1 @ 1,3: 4x4",
+            //     "#2 @ 3,1: 4x4",
+            //     "#3 @ 5,5: 2x2"
+            // };
 
-            //foreach (var claim in claims)
-            //{
-            //    solution.addclaim(claim);
-            //}
+            // foreach (var claim in claims)
+            // {
+            //     var a = solution.AddClaim(claim);
+            //     Console.WriteLine(a);
+            // }
 
-            var answer = solution.Solve();
-            Console.WriteLine(answer);
+            var s = solution.GetFabricUnderClaim();
+            solution.Solve();
+            var a = solution.SolvePart2();
+            a.ForEach(Console.WriteLine);
+            
         }
     }
 }
--- 2018/cs/AdventOfCode/Solutions/3.cs
+++ 2018/cs/AdventOfCode/Solutions/3.cs
@@ -12,7 +12,10 @@ namespace AdventOfCode.Solutions
         private int[,] _claimsGrid;
         static string claimPattern = @"#(?<id>\d+) @ (?<left>\d+),(?<top>\d+): (?<width>\d+)x(?<height>\d+)";
         Regex re = new Regex(claimPattern, RegexOptions.Compiled);
+        public List<int> UnclaimedRectangles {get; private set;} = new List<int>();
+        private List<Claim> _claims = new List<Claim>();
 
+        
         public Solution3(int size)
         {
             _size = size;
@@ -28,6 +31,14 @@ namespace AdventOfCode.Solutions
                 width = Int32.Parse(match.Groups["width"].Value),
                 height = Int32.Parse(match.Groups["height"].Value);
 
+            _claims.Add(new Claim
+                { Id = claimId,
+                  Left = left,
+                  Top = top,
+                  Width = width,
+                  Height = height
+                });
+            
             for(var i = top; i < top + height; i++)
             {
                 for(var j = left; j < left + width; j++)
@@ -44,6 +55,20 @@ namespace AdventOfCode.Solutions
                     }
                 }
             }
+        }
+
+        private bool IsUnclaimed(int left, int top, int width, int height) {
+            for (var i = top; i < top + height; ++i) {
+                for (var j = left; j < left + width; j++) {
+                    if (_claimsGrid[i, j] == -1)
+                        return false;                    
+                }
+            }
+            return true;
+        }
+
+        public bool IsUnclaimed(Claim claim) {
+            return IsUnclaimed(claim.Left, claim.Top, claim.Width, claim.Height);
         }
 
         public int GetFabricUnderClaim()
@@ -64,7 +89,7 @@ namespace AdventOfCode.Solutions
         public int Solve()
         {
             var inputPath = "inputs/3.txt";
-
+            
             using (StreamReader s = new StreamReader(File.OpenRead(inputPath)))
             {
                 while(!s.EndOfStream)
@@ -72,10 +97,30 @@ namespace AdventOfCode.Solutions
                     var claim = s.ReadLine();
                     AddClaim(claim);
                 }
-            }           
+            }
 
             var answer = GetFabricUnderClaim();
             return answer;
         }
+
+        public List<int> SolvePart2() {
+            var claims = new List<int>();
+            foreach(var claim in _claims) {
+                if (IsUnclaimed(claim)) {
+                    claims.Add(claim.Id);
+                }
+            }
+            return claims;
+        }
+            
+    
+    }
+
+    public class Claim {
+        public int Id {get; set;}
+        public int Left {get; set;}
+        public int Top {get; set;}
+        public int Width {get; set;}
+        public int Height {get; set;}
     }
 }
