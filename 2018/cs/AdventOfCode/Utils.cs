4053f01f8fc3f9de8c7b83b3b728ce02531e5621
Tushar Tyagi <tushar4r@gmail.com>
2018-12-06 06:28:46 +0530

    Update

---
+++ 2018/cs/AdventOfCode/Utils.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Utils
+{
+    public class Utils
+    {
+        public static string ReadLine(int solutionNumber) {
+            using (StreamReader s = new StreamReader(File.OpenRead($"inputs/{solutionNumber}.txt")))
+            {
+                return s.ReadToEnd().Trim();
+            }
+        }
+        
+        public static IEnumerable<string> ReadLines(int solutionNumber) {
+            using (StreamReader s = new StreamReader(File.OpenRead($"inputs/{solutionNumber}.txt")))
+            {
+                yield return s.ReadLine();
+            }
+        }
+    }
+}
