f1c29f26623353aff42dafa00952db2963d37651
Tushar Tyagi <tushar4r@gmail.com>
2019-12-01 22:02:05 -0500

    Fooling around with doctest in python

--- 2019/python/src/day1.py
+++ 2019/python/src/day1.py
@@ -2,6 +2,25 @@ from src.utils import get_inputs_as_ints
 from functools import reduce
 
 def fuel_requirements_for_module(module_mass):
+    """
+
+    Fuel required to launch a given module is based on its
+    mass. Specifically, to find the fuel required for a module, take
+    its mass, divide by three, round down, and subtract 2.
+
+    >>> fuel_requirements_for_module(12)
+    2
+
+    >>> fuel_requirements_for_module(14)
+    2
+
+    >>> fuel_requirements_for_module(1969)
+    654
+
+    >>> fuel_requirements_for_module(100756)
+    33583
+
+    """
     return (module_mass // 3) - 2
 
 def fuel_requirements_for_modules(modules):
